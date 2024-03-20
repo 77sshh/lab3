45210fdc36256e8e82f84888eab75492a4fd6ca8
Артем Колупаев <>
2024-03-20 23:49:18 +0500

    3й коммит тесты

--- TRPO-Lab3.Tests/UnitTest1.cs
+++ TRPO-Lab3.Tests/UnitTest1.cs
@@ -1,16 +1,25 @@
+using TRPO_Lab3.Lib;
 namespace TRPO_Lab3.Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+
+            const double radius = 10;
+            const double angledegrees = 90;
+            const double expected = 28.5398;
+            var area = Class1.CircleSegment(radius, angledegrees);
+            Assert.AreEqual(expected, area, 0.01);
         }
+        [Test]
+        public void Test2()
+        {
+            const double radius = -20;
+            const double angledegrees = -60;
+            Assert.Throws<Exception>(() => Class1.CircleSegment(radius, angledegrees));
+        }
+
     }
 }
