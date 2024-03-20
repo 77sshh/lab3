39d7794785936b8e441dc5e72300f46b68cc098f
Артем Колупаев <>
2024-03-20 23:42:43 +0500

    2й коммит

--- TRPO-Lab3.Lib/Class1.cs
+++ TRPO-Lab3.Lib/Class1.cs
@@ -2,6 +2,15 @@
 {
     public class Class1
     {
-
+        public static double CircleSegment(double radius, double angledegrees)
+        {
+            if (radius <= 0 || angledegrees <= 0)
+            {
+                throw new Exception("Радиус и угол должны быть заданы положительным числом");
+            }
+            double angleradians = angledegrees * Math.PI / 180;
+            double area = (Math.Pow(radius, 2) / 2) * (angleradians - Math.Sin(angleradians));
+            return area;
+        }
     }
 }
