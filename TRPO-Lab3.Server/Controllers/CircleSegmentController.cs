c75dd4c8d6ab23eb5438cbc6379e2c94639428cf
Артем Колупаев <>
2024-04-10 00:08:38 +0500

    8й коммит 5ая лаба

---
+++ TRPO-Lab3.Server/Controllers/CircleSegmentController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TRPO_Lab3.Server.Models;
+
+namespace TRPO_Lab3.Server.Controllers
+{
+    public class CircleSegmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Calculate(CircleSegmentModel model)
+        {
+            // Проверка модели на валидность
+            if (!ModelState.IsValid)
+            {
+                return View("Index", model);
+            }
+
+            try
+            {
+                // Вычислить площадь сегмента круга на основе модели
+                double area = Lib.Class1.CircleSegment(model.radiu5, model.angle);
+
+                // Передать результат в представление
+                ViewBag.Area = area;
+            }
+            catch (Exception ex)
+            {
+                // Обработка исключения, если радиус или угол отрицательные
+                ViewBag.ErrorMessage = ex.Message;
+            }
+
+            return View("Index");
+        }
+    }
+}
---
+++ TRPO-Lab3.Server/Models/CircleSegmentModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace TRPO_Lab3.Server.Models
+{
+    public class CircleSegmentModel
+    {
+        [DisplayName("Радиус")]
+        public double radiu5 { get; set; }
+        [DisplayName("Угол(градусы)")]
+        public double angle { get; set; }
+    }
+}
